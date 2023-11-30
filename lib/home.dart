86cdcfa134222c9e9ec10243cc6a32959238ce19
tungnv <nguyenvantung2001dn@gmail.com>
2023-11-30 15:05:18 +0700

    Validate login form

---
+++ lib/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Home",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.blue,
+      )
+    );
+  }
+}
